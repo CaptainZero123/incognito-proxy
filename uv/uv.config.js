23d63fe870c220448fad348baabea21bba78b6fc
galaxy4627 <100154768+galaxy4627@users.noreply.github.com>
2022-04-24 11:14:04 -0400

    Update uv.config.js

--- uv/uv.config.js
+++ uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/service/',
-    bare: 'http://35.237.181.245:80/',
+    bare: 'https://incog.dev/bare/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/uv/uv.handler.js',
