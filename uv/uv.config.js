26674e2bfbed7f93994ece087d53a956e471b510
CaptainZero123 <134757214+CaptainZero123@users.noreply.github.com>
2024-02-29 20:43:30 -0600

    Update uv.config.js

--- uv/uv.config.js
+++ uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/service/',
-    bare: 'https://math-op.vercel.app/bare/',
+    bare: 'https://astroidv3-self.vercel.app/bare/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/uv/uv.handler.js',
